1eeb9b5c9cf631e1abb31b6e064f19c8bb1807b9
EmanuelCenturion <emanuelcenturion82@gmail.com>
2020-07-16 12:46:37 -0300

    Cambio desde Consola

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,10 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+var vNombre;
 function mostrar()
 {
-	alert("ok");
+	vNombre = prompt("Nombre","ingrese su nombre")
+	alert(vNombre);
 }
 
