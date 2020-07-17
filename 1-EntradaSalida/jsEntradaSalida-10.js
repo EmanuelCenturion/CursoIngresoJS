df4eb27ea2edfdf8a9992beca7f8385f66454d48
EmanuelCenturion <emanuelcenturion82@gmail.com>
2020-07-17 17:58:24 -0300

    Practica en casa

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,10 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var vImporte;
+	var vDescuento;
+	vImporte = document.getElementById("txtIdImporte").value;
+	vImporte = parseInt(vImporte);
+	vDescuento = vImporte - (vImporte*0.25);
+	document.getElementById("txtIdResultado").value = vDescuento;
 }
