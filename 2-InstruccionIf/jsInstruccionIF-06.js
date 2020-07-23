fbe816173041b5e63017c51b013142dff8a4bfec
EmanuelCenturion <emanuelcenturion82@gmail.com>
2020-07-23 19:28:42 -0300

    Practica en casa

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -5,9 +5,9 @@ function mostrar()
 	vEdad = parseInt(document.getElementById("txtIdEdad").value);
 	if(vEdad<13){
 		alert("Es niño");
-	}else if(vEdad>=13 && vEdad<=17){
+	}else if(vEdad<18){
 		alert("Es adolescente");
 	}else{
 		alert("Es mayor de edad");
-}
+	}	
 }//FIN DE LA FUNCIÓN
